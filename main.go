0f6fe05132e806aa8ce694e1c55f425a5265a484
Manfred Touron <m@42.am>
2019-05-27 17:27:43 +0200

    feat: support loading key from a file

--- main.go
+++ main.go
@@ -12,7 +12,11 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to read stdin: %v", err)
 	}
+
 	key := []byte(os.Args[1])
+	if dat, err := ioutil.ReadFile(string(key)); err != nil { // if key is a path, read the key from this file
+		key = dat
+	}
 
 	if len(plaintext) != len(key) {
 		log.Fatalf("plaintext and key should have the same length (%d != %d)", len(plaintext), len(key))
